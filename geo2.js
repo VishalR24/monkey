49821e71b07dab183f82e646d47158aecbf9a805
VishalR24 <68307996+VishalR24@users.noreply.github.com>
2020-07-14 22:40:05 -0400

    add files via upload

---
+++ geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "MARLBOROUGH",
+      'continent': "NA",
+      'country': "US",
+      'region': "MA"
+    },
+    'ip':"96.237.159.222"
+  }]);
+})
+//
+()
+
+;
